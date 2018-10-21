6b1d25dafed3f66ffc65adf6f056bf8e1cde0cc1
user <user@example.com>
2018-10-21 11:01:04 +0530

    add debug output

--- telbot.rb
+++ telbot.rb
@@ -91,15 +91,21 @@ end
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   sender = Sender.new(bot)
+  logger.debug "bot cycle"
   bot.listen do |message|
+    logger.debug "get message: #{message.inspect}"
     case message.text
     when '/start'
+      logger.debug "message start"
       sender.start(message)
     when '/stop'
+      logger.debug "message stop"
       sender.stop(message)
     when '/more'
+      logger.debug "message more"
       sender.more(message)
     else
+      logger.debug "message else"
       sender.request(message)
     end
   end
