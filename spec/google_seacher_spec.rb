85d89c8113b17b8604d9d5093852e8c94b0328e2
Цыганов Антон <you@example.com>
2018-11-02 08:57:01 +0300

    modify test

--- spec/google_seacher_spec.rb
+++ spec/google_seacher_spec.rb
@@ -1,14 +1,11 @@
 require './lib/google_seacher'
 
 describe GoogleSeacher do
-  before(:each) do
-    @google_seacher = GoogleSeacher.new('cats')
-  end
-
-  it 'should return link' do
+  let(:google_seacher) { GoogleSeacher.new('cats') }
+  it 'return link' do
     10.times do
-      link = @google_seacher.next
-      expect(link).to match(%r{http[s]?://.+})
+      link = google_seacher.next
+      expect(link).to match %r{http[s]?://.+}
     end
   end
 end
